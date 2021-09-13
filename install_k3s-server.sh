158bcc9c0b6295c330152e85d10146751e19209d
JmbFountain <jmb@smx.de>
2021-09-13 15:13:36 +0200

    started adding rancher

--- install_k3s-server.sh
+++ install_k3s-server.sh
@@ -7,7 +7,8 @@ chmod +x setup_script
 ./setup_script --cluster-cidr "172.30.0.0/16" \
 		  			--service-cidr "172.31.0.0/16" \
 		  			--cluster-dns  "172.31.0.10" \
-		  			--cluster-domain "nathan.home"
+		  			--cluster-domain "nathan.home" \
+		  			--disable traefik --disable servicelb
 
 # Add helm repos for services
 helm repo add metallb https://metallb.github.io/metallb
@@ -22,12 +23,25 @@ kubectl create namespace longhorn-system
 helm install longhorn longhorn/longhorn --namespace longhorn-system
 
 # Install Kubernetes Dashboard
-GITHUB_URL=https://github.com/kubernetes/dashboard/releases
-VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
-k3s kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
 k3s kubectl create -f dashboard.admin-user.yml -f dashboard.admin-user-role.yml
 
 echo "--- KUBERNETES CONFIG ---"
-more /etc/rancher/k3s/k3s.yaml | sed -e "s/127\.0\.0\.1/$(hostname)/" | tee kubeconfig
+cat /etc/rancher/k3s/k3s.yaml | sed -e "s/127\.0\.0\.1/$(hostname)/" | tee kubeconfig
 echo "--- DASHBOARD TOKEN ---"
 k3s kubectl -n kubernetes-dashboard describe secret admin-user-token | grep '^token' | awk '{print $2}'  | tee token.txt
+
+# installing Racher Namespaces
+kubectl create namespace cattle-system
+kubectl create namespace cert-manager
+
+# instsalling cert-manager
+kubectl apply -f cert-manager.crds.yaml
+helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install cert-manager jetstack/cert-manager --namespace cert-manager
+
+# installing Rancher
+helm install rancher rancher-latest/rancher --namespace cattle-system --set hostname=k3s-rancher.nathan.home
+kubectl -n cattle-system rollout status deploy/rancher
+kubectl -n cattle-system get deploy rancher
